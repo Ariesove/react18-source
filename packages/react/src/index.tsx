eacc7732576f1f43b7c6b1430915d35d1703b554
Ariesove <1980114332@qq.com>
2023-06-27 22:22:17 +0800

    feat: jsx 函数创建

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -19,7 +19,43 @@ const ReactElement = function (
 		key,
 		ref,
 		props,
+		// 标记模仿的虚拟DOM元素
 		__mark: 'cxsong'
 	};
 	return element;
+};
+// 构建jsx,其实这个就类似于react.createlement
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+	// 这里和原有逻辑不一致暂时不清楚为什么
+	for (const prop in config) {
+		if (!config.hasOwnProperty(prop)) {
+			continue;
+		}
+
+		const val = config[prop];
+
+		if (prop === 'key' && val !== undefined) {
+			key = String(val);
+			continue;
+		}
+
+		if (prop === 'ref' && val !== undefined) {
+			ref = val;
+			continue;
+		}
+
+		props[prop] = val;
+	}
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+	return ReactElement(type, key, ref, props);
 };
